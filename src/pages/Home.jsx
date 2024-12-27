287149960cf91601d8781ef4d7ff67095e7c2df0
charleshenrico <ccvilla1>
2024-12-27 16:10:27 +0800

    Live and Hone

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,62 @@
 import React from 'react';
 import '/src/styles/Home.css';
-import banner from '/src/assets/HEADER.png' 
-import image1 from '/src/assets/IMAGE 1.png'
-import image2 from '/src/assets/IMAGE 2.png' 
-import image3 from '/src/assets/IMAGE 3.png'  
-import image4 from '/src/assets/IMAGE 4.png' 
-import image5 from '/src/assets/IMAGE 5.png' 
-import image6 from '/src/assets/IMAGE 6.png' 
+import image1 from '/src/assets/Image1.jpg' 
+import image2 from '/src/assets/Image2.jpg' 
+import image3 from '/src/assets/Image3.jpg' 
+import image4 from '/src/assets/Image4.jpg' 
+import image5 from '/src/assets/Image5.jpg' 
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import MainHeader from '../components/Header';
 
 function Home() {
   return (
-    <div>
+    <div className="Home-Page">
 
-    <div className="home-container">
 
-      <section className="hero" style={{ backgroundImage: `url(${banner})` }}>
-        <h1>JOIN US IN SPREADING GOD’S WORD</h1>
-      </section>
+      <div className="Home-Top">
+  <div className="Home-Top-Text">
+    <h1>GOD.</h1>
+    <h1>FAMILY. NATIONS.</h1>
+  </div>
+</div>
 
-      <main className="main-content">
-        <section className="events">
-          <div className="event">
-            <img src={image4} alt="DMI Charity Event 2024" />
-            <p>DMI CHARITY EVENT 2024</p>
-          </div>
-          <div className="event">
-            <img src={image5} alt="Community Help" />
-          </div>
-          <div className="event">
-            <img src={image2} alt="Church Renovation" />
-            <p>CHURCH RENOVATION</p>
-          </div>
-          <div className="event">
-            <img src={image1} alt="Neighborhood Feeding Program" />
-          </div>
-        </section>
-      </main>
-      
+
+      <div className="Home-Body-1">
+  <div className="image-container">
+  <h2>2008</h2>
+  <h3>STARTED IN</h3>
+    <img class="image1" src={image1} alt="Description of image" width="400" height="400" />
+    <img class="image2" src={image2} alt="Description of image" width="400" height="400" />
+  </div>
+  <div className="text-container">
+  <h2>Who we are</h2>
+  <h1>DMI Description</h1>
+  <h5>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  </h5>
+  <button className="about-us-button">About Us</button>
+</div>
+</div>
+
+      <div className="Home-Body-2">
+      <h5>DMI CHURCH</h5>
+      <h1> Here at Destiny Ministry International, Lorem ipsum dolor sit amit, consectetur adipiscing elit.</h1>
+      <h5>Joseh Solangon</h5>
+      <h4>HEAD PASTOR</h4>
+      </div>
+
+      <div className="Home-Body-3">
+      <img className = "display-1" src={image3} alt="Description of image" width="300" height="300"/>
+      <img className = "display-2" src={image4} alt="Description of image" width="300" height="300"/>
+      <img className = "display-3" src={image5} alt="Description of image" width="300" height="300"/>
+      </div>
+
+
     </div>
-    </div>
-
   );
 }
 
 
 export default Home;
+
--- src/pages/Live.jsx
+++ src/pages/Live.jsx
@@ -1,10 +1,57 @@
 import React from "react";
 import Footer from "../components/Footer";
 import MainHeader from "../components/Header";
+import '/src/styles/Live.css'; 
 
 function Live() {
+  const videos = [
+    { src: "https://www.youtube.com/embed/rONI7KgM_IU", date: "December 22, 2024" },
+    { src: "https://www.youtube.com/embed/mHXRUvxNKFw", date: "December 21, 2024" },
+    { src: "https://www.youtube.com/embed/FbL65RNnPbk", date: "December 20, 2024" }
+  ];
+  
+
+
   return (
-    <div>
+    <div className="Live-Page">
+
+
+      <div className="LiveVid">
+        <h1>SUNDAY SERVICE</h1>
+        <h5>December 22, 2024</h5>
+        <iframe
+          width="800"
+          height="480"
+          src="https://www.youtube.com/embed/Zu7jurElcIM"
+          style={{ border: "none", borderRadius: "15px"  }}
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="DMI Live Video"
+        ></iframe>
+      </div>
+
+
+
+      <div className="PreviousVid">
+      <h1>PREVIOUS LIVES</h1>
+        <div className="scroll-container">
+          {videos.map((video, index) => (
+            <div key={index} className="video-item">
+              <iframe
+                width="400"
+                height="250"
+                src={video.src}
+                style={{ border: "none", borderRadius: "15px"  }}
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title={`DMI Live Video - ${index}`}
+              ></iframe>
+              <h5>{video.date}</h5>
+            </div>
+          ))}
+        </div>
+      </div>
+
 
     </div>
   );
